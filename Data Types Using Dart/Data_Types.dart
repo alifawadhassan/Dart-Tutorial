fc224aa88d7535b8ce025602e7b8ea437e96323f
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-18 23:13:02 +0500

    Add files via upload

---
+++ Data Types Using Dart/Data_Types.dart
@@ -0,0 +1,14 @@
+void main() 
+{
+  int a = 10;
+  print("Integer : ${a}");
+
+  double b = 10.5;
+  print("Double : ${b}");
+  
+  String c = "Ali Fawad";
+  print("String : ${c}");
+  
+  bool d = true;
+  print("Boolean : ${d}");
+}
