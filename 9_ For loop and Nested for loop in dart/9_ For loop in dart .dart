e87abb88c4d745aeca24909c9c04a8074ceddbbf
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-20 19:28:17 +0500

    Add files via upload

---
+++ 9_ For loop and Nested for loop in dart/9_ For loop in dart .dart
@@ -0,0 +1,7 @@
+void main() 
+{
+    for(int j=0;j<3;j++)
+    {
+      print("j= $j");
+    }
+}  
---
+++ 9_ For loop and Nested for loop in dart/9_ Nested for loop in dart .dart
@@ -0,0 +1,11 @@
+void main() 
+{
+  for(int i=0;i<4;i++)
+  {
+    print("Value of i = $i");
+    for(int j=0;j<3;j++)
+    {
+      print("j= $j");
+    }
+  }
+}  
