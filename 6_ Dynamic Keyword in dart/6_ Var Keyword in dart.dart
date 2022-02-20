a28b5a59cf82e03065e25e8dc978583f2038550c
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-20 17:47:08 +0500

    Add files via upload

---
+++ 6_ Dynamic Keyword in dart/6_ Var Keyword in dart.dart
@@ -0,0 +1,10 @@
+void main() 
+{
+  dynamic a= 4;
+  print(a);
+  
+  a="Ali";
+  print(a);
+  
+  //Difference between var and dynamic is that you can change dynamically values of variables easily by using dynamic keyword
+}
