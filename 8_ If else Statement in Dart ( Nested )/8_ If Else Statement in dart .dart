9d90645fe1f376dd993e13e06e3d3775398acefa
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-20 19:20:32 +0500

    Add files via upload

---
+++ 8_ If else Statement in Dart ( Nested )/8_ If Else Statement in dart .dart
@@ -0,0 +1,11 @@
+void main() 
+{
+  String a="Ali";
+  
+  if(a=="Ali")
+  {
+    print("$a is a good boy");
+  }
+  else
+    print("Bad boy");
+}  
---
+++ 8_ If else Statement in Dart ( Nested )/8_ Nested If Else Statement in dart .dart
@@ -0,0 +1,21 @@
+void main() 
+{
+  int a=25;
+  
+  if(a<10)
+  {
+    print("a is less than 10"); 
+  }
+  else if(a<20)
+  {
+    print("a is less than 20"); 
+  }
+  else if(a<30)
+  {
+    print("a is less than 30"); 
+  }
+  else
+  {
+    print("Not Valid");
+  }
+}  
