f1230c58d13b8cbfc98b32b9442f6c7144ae9349
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-20 17:26:22 +0500

    Add files via upload

---
+++ 4_ String Interpolation and Concatination/String Interpolation and Concatination .dart
@@ -0,0 +1,14 @@
+void main() 
+{
+  String a = "Ali";
+  
+  print("My Name is "+a);
+  
+  print("My Name is "+"Ali");
+  
+  //for printing simple variable we use $ with variable name
+  print("My Name is $a");
+  
+  //curly braces used when we want to use function
+  print("Length of my name is ${a.length}");
+}
