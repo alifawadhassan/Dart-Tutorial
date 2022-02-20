80a9104a9e6be83a28988148b66e7fd4a85d52fe
Ali Fawad Hassan <90863461+alifawadhassan@users.noreply.github.com>
2022-02-20 19:53:17 +0500

    Add files via upload

---
+++ 13_ Switch Statement in dart/13_ Switch Statement in dart .dart
@@ -0,0 +1,23 @@
+void main() 
+{
+  String colour= "black";
+  print("Switch Statement: \n");
+  
+  switch(colour)
+  {
+    case "red":
+      print("color is red");
+      break;
+      
+    case "green":
+      print("color is green");
+      break;
+      
+    case "black":
+      print("color is black");
+      break;
+      
+    default:
+      print("Invalid Color");
+  }
+}  
