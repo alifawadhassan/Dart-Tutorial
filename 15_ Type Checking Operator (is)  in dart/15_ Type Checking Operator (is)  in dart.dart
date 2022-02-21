fa6eeaee03d52f4984ce74db876d14c11db4aa58
Ali Fawad Hassan <00alifawad@gmail.com>
2022-02-21 08:49:22 +0500

    Add files via upload

---
+++ 15_ Type Checking Operator (is)  in dart/15_ Type Checking Operator (is)  in dart.dart
@@ -0,0 +1,10 @@
+void main() 
+{
+  var a=5;
+  
+  //print true if datatype is same as mentioned, else print false
+  print(a is int);  
+  
+  //print true if datatype is not same as mentioned, else print false
+  print(a is! int);
+}  
